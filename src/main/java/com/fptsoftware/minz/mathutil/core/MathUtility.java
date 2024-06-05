d23768facac095a008f10841de5dd98ce607df27
minhduy77 <duyle200311a1@gmail.com>
2024-06-05 19:45:23 +0700

    red alert

--- src/main/java/com/fptsoftware/minz/mathutil/core/MathUtility.java
+++ src/main/java/com/fptsoftware/minz/mathutil/core/MathUtility.java
@@ -24,7 +24,7 @@ public class MathUtility {
     //0! = 1! = 1
     // n = 0...20! vì n = 21! vì n = 21! vượt số 0, 18 số 0 là vừa đủ long
     public static long getFactorial(int n) {
-        long product = 1; //tính kết quả phép nhân
+        long product = 10; //tính kết quả phép nhân
         
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20 ");
